20894034592b131064f160946ebe63c8ca1507a3
Michael Gipson <mgipsy101@gmail.com>
2020-10-27 09:38:43 -0400

    Delete players done.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -13,6 +13,29 @@ class PlayersController < ApplicationController
 			redirect_to "/players/new"
 		end
 	end
+	def show
+
+		@player = Player.find(params[:id])
+		
+
+		end
+	def edit
+		@player = Player.find(params[:id])
+	end
+	def update
+		player = Player.find(params[:id])
+		if player.update(player_params)
+			redirect_to "/players"
+		else
+			flash[:errors] = player.errors.full_messages
+			redirect_to "/players/#{player.id}/edit"
+		end
+	end	
+	def destroy
+		player = Player.find(params[:id])
+		player.destroy
+		redirect_to "/players"
+	end
 	private
 	def player_params
 		params.require(:player).permit(:first_name, :last_name, :season, :goals, :assists)
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 	get "players" => "players#index"
 	get "players/new" => "players#new"
 	post "players" => "players#create"
+	get "players/:id/edit" => "players#edit"
+  	patch "players/:id" => "players#update"
+  	get "players/:id" => "players#show"
+  	delete "players/:id" => "players#destroy"
 end
